b029801fde223cdd98d6fade52b29d50786b7a28
Julio Hernandez <jcmex15@gmail.com>
2024-10-14 09:35:05 -0600

    1

--- Dictionaries.py
+++ Dictionaries.py
@@ -195,7 +195,9 @@ for alien in aliens[0:5]:
 print("...")
 print("\n")
 
-## A List in a Dictionar
+#############################
+#### A List in a Dictionar###
+#############################
 # Store information about a pizza being ordered.
 pizza = {
     "crust": "thick",
@@ -220,3 +222,25 @@ for name, languages in favorite_languages.items():
     for language in languages:
         print("\t" + language.title())
 print("\n")
+
+####################################
+#### A Dictionary in a Dictionary###
+####################################
+users = {
+    "aeinstein": {
+        "first": "albert",
+        "last": "einstein",
+        "location": "princeton",
+    },
+    "mcurie": {
+        "first": "marie",
+        "last": "curie",
+        "location": "paris",
+    },
+}
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info["first"] + " " + user_info["last"]
+    location = user_info["location"]
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
---
+++ Functions.py
@@ -0,0 +1,16 @@
+##Ejemplo
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+
+
+greet_user()
+
+
+## Passing Information to a Function
+def greet_user(username):
+    """Display a simple greeting."""
+    print("Hello, " + username.title() + "!")
+
+
+greet_user("jesse")
