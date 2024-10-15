82a80a77ffe6ee383172e80b572312c8c3c4febe
Julio Hernandez <jcmex15@gmail.com>
2024-10-15 13:08:33 -0600

    1

--- Functions.py
+++ Functions.py
@@ -14,3 +14,117 @@ def greet_user(username):
 
 
 greet_user("jesse")
+
+################################
+##### Passing Arguments
+#################################
+
+
+## Positional Arguments
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+describe_pet("hamster", "harry")
+
+## Multiple Function Calls
+describe_pet("dog", "willie")
+
+## Keyword Arguments
+describe_pet(animal_type="cat", pet_name="corina")
+
+
+##Default Values
+def describe_pet(pet_name, animal_type="dog"):
+    """Display information about a pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+describe_pet(pet_name="willie")
+describe_pet(pet_name="harry", animal_type="hamster")
+print("\n")
+
+##########################
+######## Return Values
+##########################
+
+
+## Returning a Simple Value
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = first_name + " " + last_name
+    return full_name.title()
+
+
+musician = get_formatted_name("jimi", "hendrix")
+print(musician, " \n")
+
+
+## Making an Argument Optional
+def get_formatted_name(first_name, last_name, middle_name=""):
+    """Return a full name, neatly formatted."""
+    if middle_name:
+        full_name = first_name + " " + middle_name + " " + last_name
+    else:
+        full_name = first_name + " " + last_name
+    return full_name.title()
+
+
+musician = get_formatted_name("jimi", "hendrix")
+print(musician)
+musician = get_formatted_name("john", "hooker", "lee")
+print(musician, "\n")
+
+
+## Returning a Dictionary
+def build_person(first_name, last_name):
+    """Return a dictionary of information about a person."""
+    person = {"first": first_name, "last": last_name}
+    return person
+
+
+musician = build_person("jimi", "hendrix")
+print(musician, "\n")
+
+
+#### Returning a Dictionary
+def build_person(first_name, last_name, age=""):
+    """Return a dictionary of information about a person."""
+    person = {"first": first_name, "last": last_name}
+    if age:
+        person["age"] = age
+    return person
+
+
+musician = build_person("jimi", "hendrix", age=27)
+print(musician, "\n")
+
+
+## Using a Function with a while Loop
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = first_name + " " + last_name
+    return full_name.title()
+
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+
+    f_name = input("First name: ")
+    if f_name == "q":
+        break
+    l_name = input("Last name: ")
+    if l_name == "q":
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print("\nHello, " + formatted_name + "!")
+
+
+####################
+## pasing a list
+####################
